add760c42b85191890476fb02724511be933d179
virmerson <virmerson@htcursos.com>
2020-07-29 12:21:32 -0400

    DI entre classes

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-const awilix = require("awilix")
+import awilix from 'awilix'
+
 const container = awilix.createContainer();
-console.log(container)
+
+class UserController {
+    
+    constructor({userService}) { 
+        console.log("Instanciando UserController")
+        this.userService = userService
+    }
+  
+    getUser() {
+      return this.userService.getUser()
+    }
+  }
+
+  class UserService{
+
+        constructor(){
+                console.log("Instanciando Service")
+        }
+
+        getUser(){
+            //Fez busca com repository
+            return {name:"Jão", email:"jao@gmail.com"}
+        }
+
+  }
+
+
+  container.register({
+   
+    userController: awilix.asClass(UserController),
+    userService:awilix.asClass(UserService)
+  })
+  
+
+
+ const controller = container.resolve("userController")
+ console.log(controller.getUser())
+
+  
